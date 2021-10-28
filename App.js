98b05fe2e907cb54d1319a45a293615a7c18e22b
Arthur Yan <arthuryan08@gmail.com>
2021-10-28 09:58:00 -0300

    Update App.js

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#222",
+    color: "white",
     alignItems: "center",
     justifyContent: "center",
   },
